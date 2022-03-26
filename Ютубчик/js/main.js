14dca02e62bd0cf34ba4953e7f4aea42c3ca8b10
SimonovIvan1 <97295716+SimonovIvan1@users.noreply.github.com>
2022-03-26 22:16:48 +0300

    Add files via upload

---
+++ Ютубчик/js/main.js
@@ -0,0 +1,93 @@
+const MyChannelSlaider = new Swiper('.MyChannelSlaider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+1900: {
+  slidesPerView: 6,
+},
+1600: {
+  slidesPerView: 5,
+},
+1300: {
+  slidesPerView: 4,
+},
+1100: {
+  slidesPerView: 3,
+},
+800: {
+  slidesPerView: 2,
+},
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '#my-channel-button-next',
+    prevEl: '#my-channel-button-prev',
+  },
+ 
+});
+
+const RecommendedSlider = new Swiper('.Recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints:{
+1700:{
+  slidesPerView: 3,
+},
+1100:{
+  slidesPerView: 2,
+},
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '#recommended-button-next',
+    prevEl: '#recommended-button-prev',
+  },
+ 
+});
+
+const RecommendedChannel = new Swiper('.Recommended-channel', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+1900: {
+  slidesPerView: 6,
+},
+1600: {
+  slidesPerView: 5,
+},
+1300: {
+  slidesPerView: 4,
+},
+1100: {
+  slidesPerView: 3,
+},
+800: {
+  slidesPerView: 2,
+},
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '#recommended-channel-button-next',
+    prevEl: '#recommended-channel-button-prev',
+  },
+ 
+});
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group')
+searchBtn.addEventListener('click', () =>{
+mobileSearch.classList.toggle('is-open');
+}
+);
+
+if (document.documentElement.scrollWidth <= 640) {
+  MyChannelSlaider.destroy();
+  RecommendedSlider.destroy();
+RecommendedChannel.destroy();
+
+}
